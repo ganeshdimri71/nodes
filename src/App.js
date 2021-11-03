14fd62be2b40b6e6eda9846fa4d1de9e88d6ac09
ganesh-gamd <dimri@gamorgan.in>
2021-11-03 17:43:28 +0530

    tyyyyyyyy

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Points from "./Points";
 import Nodes from "./Nodes";
 import Nodes1 from "./Nodes1";
+import ClassNodes1 from "./ClassNodes1";
 
 function App() {
 	return (
 		<div className="App">
 			{/* <Points /> */}
 			{/* <Nodes1 /> */}
-			<Nodes1 />
+			{/* <Nodes1 /> */}
+			<ClassNodes1 />
 		</div>
 	);
 }
