c4dbda54ceec6d96465508c0c56efd1ecfb62476
ganesh-gamd <dimri@gamorgan.in>
2021-11-04 16:52:54 +0530

    ccccc

--- src/ClassNodes1.js
+++ src/ClassNodes1.js
@@ -17,27 +17,24 @@ import {
 function ClassNodes1() {
 	const [nodes, setNodes] = useState([]);
 	const [currentKey, setCurrentKey] = useState("a");
-	const [currentKeyValue, setCurrentKeyValue] = useState([1]);
-	const [currentKeyValueOne, setCurrentKeyValueOne] = useState([1]);
+	const [currentKeyValue, setCurrentKeyValue] = useState(0);
+	const [currentKeyValueOne, setCurrentKeyValueOne] = useState([]);
 
 	const handleClickOnStage = (e) => {
 		let pos = e.target.getStage().getPointerPosition();
 		setNodes([
 			...nodes,
-			[pos.x, pos.y, currentKey + currentKeyValueOne.length],
-		]);
+			[pos.x, pos.y, currentKey +( currentKeyValueOne.length+1)],
+		] );
+		console.log( currentKey );
 		let temp = currentKeyValueOne;
 		temp.push(1);
 		setCurrentKeyValueOne(temp);
 	};
 
-	const keyPressEventHandler = (e) => {
-		console.log(
-			"The value of the length of the current key value is : ",
-			currentKeyValue.length
-		);
-		console.log("Hi I am key Press");
-		console.log("The value of the nodes is : ", nodes);
+	const keyPressEventHandler = ( e ) => {
+		console.log(e)
+		var KEYCODE_ESC = 27;
 		const arr = [
 			"a",
 			"b",
@@ -66,84 +63,66 @@ function ClassNodes1() {
 			"y",
 			"z",
 		];
-		if (currentKeyValue.length <= 25) {
-			console.log("Hi I am also key Press");
-			let temp = currentKeyValue;
-
-			setCurrentKeyValue(temp);
-			setCurrentKey(arr[currentKeyValue.length]);
-			temp.push(1);
-			setCurrentKeyValueOne([1]);
-		} else {
-			let temp = currentKeyValue;
-			temp.splice(0, temp.length);
-			setCurrentKeyValue(temp);
+		if ( e.keyCode === KEYCODE_ESC ) {
+			console.log("current key ", currentKey)
+			let index = arr.indexOf( currentKey );
+			console.log('index', index)
+			console.log(arr[index + 1]);
+			setCurrentKey( arr[ index + 1 ] )
+			setCurrentKeyValueOne([])
 		}
-		// var KEYCODE_ESC = 27;
-		// if (e.keyCode === KEYCODE_ESC ) {
-		// 	let temp = currentKeyValue;
-		// 	temp.push(1);
-		// 	setCurrentKeyValue(temp);
-
-		// 	setCurrentKey(arr[currentKeyValue.length]);
-		// 	setCurrentKeyValueOne([1]);
-		// } else {
-		// 	setCurrentKeyValue(-1);
-		// }
+	
 	};
 
-	useEffect(() => {
-		window.addEventListener("keydown", keyPressEventHandler);
-		console.log("I am mounted...!");
-
-		return () => window.removeEventListener("keydown", keyPressEventHandler);
-	}, []);
+	
 
 	return (
 		<>
-			<div id="container"></div>
-			<Stage
-				width={window.innerWidth}
-				height={window.innerHeight}
-				container="container"
-				onClick={handleClickOnStage}
-			>
-				<Layer>
-					<Group>
-						{nodes.map((node, i) => {
-							return (
-								<>
-									<Text
-										key={new Date().getMilliseconds()}
-										x={node[0] - 9}
-										y={node[1] - 23}
-										text={node[2]}
-										fill="grey"
-									/>
-									<Circle
-										key={`${node[1]}`}
-										x={node[0]}
-										y={node[1]}
-										fill="black"
-										radius={10}
-										shadowBlur={5}
-									/>
-									<Circle
-										key={`${node[2]}`}
-										x={node[0]}
-										y={node[1]}
-										fill="white"
-										radius={5}
-										shadowBlur={5}
-									/>
-								</>
-							);
-						})}
-					</Group>
-				</Layer>
-			</Stage>
+			<div id="container" tabIndex="0" onKeyDown={(e) => keyPressEventHandler(e)}>
+				<Stage
+					width={window.innerWidth}
+					height={window.innerHeight}
+					container="container"
+					onClick={handleClickOnStage}
+				>
+					<Layer>
+						<Group>
+							{nodes.map((node, i) => {
+								return (
+									<>
+										<Text
+											key={shortid.generate()}
+											x={node[0] - 9}
+											y={node[1] - 23}
+											text={node[2]}
+											fill="grey"
+										/>
+										<Circle
+											key={Math.random()}
+											x={node[0]}
+											y={node[1]}
+											fill="black"
+											radius={10}
+											shadowBlur={5}
+										/>
+										<Circle
+											key={`${node[2]}`}
+											x={node[0]}
+											y={node[1]}
+											fill="white"
+											radius={5}
+											shadowBlur={5}
+										/>
+									</>
+								);
+							})}
+						</Group>
+					</Layer>
+				</Stage>
+			</div>
 		</>
 	);
 }
 
 export default ClassNodes1;
+
